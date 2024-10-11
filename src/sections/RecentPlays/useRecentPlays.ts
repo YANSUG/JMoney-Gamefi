14e6ae1a13d6f90f95a792a8189da29d04196dd0
YANSUG <heo1557424@gmail.com>
2024-10-11 12:39:47 +0800

    Update useRecentPlays.ts

--- src/sections/RecentPlays/useRecentPlays.ts
+++ src/sections/RecentPlays/useRecentPlays.ts
@@ -16,7 +16,7 @@ export function useRecentPlays(params: Params = {}) {
   // Fetch previous events
   const previousEvents = useGambaEvents(
     'GameSettled',
-    { address: AjhV5Su8V5NLsnfqkYvUTGSRMPkj9pQ33HTohFAU8R43 },
+    { address: !showAllPlatforms ? PLATFORM_CREATOR_ADDRESS : AjhV5Su8V5NLsnfqkYvUTGSRMPkj9pQ33HTohFAU8R43  },
   )
 
   const [newEvents, setEvents] = React.useState<GambaTransaction<'GameSettled'>[]>([])
