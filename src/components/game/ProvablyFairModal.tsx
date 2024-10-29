ab388b3e18d22ed96dcd5c5a246f7c82e997f71f
YANSUG <heo1557424@gmail.com>
2024-10-29 23:11:37 +0800

    Update ProvablyFairModal.tsx

--- src/components/game/ProvablyFairModal.tsx
+++ src/components/game/ProvablyFairModal.tsx
@@ -22,22 +22,19 @@ export function ProvablyFairModal(props: { onClose: () => void }) {
 
   return (
     <Modal onClose={() => props.onClose()}>
-      <h1 className="text-2xl font-bold">Provably Fair</h1>
+      <h1 className="text-2xl font-bold">可證公平性</h1>
       {!gamba.userCreated && (
         <>
           <p>
-            Provably Fair allows you to verify that the result of each game was
-            randomly generated. Since you are playing from this wallet for the
-            first time, you can request the initial hashed seed ahead of time.
-            After this it will be done automatically for each play.
+            可證公平性讓您可以驗證每場遊戲的結果都是隨機生成的。由於您是第一次使用這個錢包遊玩，您可以提前請求初始的哈希種子。之後每次遊玩時將自動完成此步驟。
           </p>
-          <GambaPlayButton onClick={initialize} text="Get hashed seed" />
+          <GambaPlayButton onClick={initialize} text="取得哈希種子" />
         </>
       )}
       {gamba.userCreated && (
         <>
           <p>
-            Your client seed will affect the result of the next game you play.
+            您的客戶端種子將會影響您下一場遊戲的結果。
           </p>
           <div
             style={{
@@ -47,9 +44,9 @@ export function ProvablyFairModal(props: { onClose: () => void }) {
               padding: "20px",
             }}
           >
-            <div>Next RNG Seed (sha256)</div>
+            <div>下一次隨機數種子 (sha256)</div>
             <GambaUi.TextInput value={gamba.nextRngSeedHashed || ""} disabled />
-            <div>Client Seed</div>
+            <div>客戶端種子</div>
             <div style={{ display: "flex", gap: "10px", width: "100%" }}>
               <GambaUi.TextInput
                 style={{ flexGrow: "1" }}
